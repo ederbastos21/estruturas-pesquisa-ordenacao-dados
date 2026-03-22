ed9b7fae2a4102316f39dd71c7a1c863ee0fe228
eder <ederbastos.dev@gmail.com>
2026-03-22 15:40:46 -0300

    mudar as chamadas de funcao da main para corresponder as mudancas feitas dentro da classe BinarySearchTree, alem de formatar o codigo para melhor visualização

--- src/main/java/br/unicesumar/projeto_1/bst/Main.java
+++ src/main/java/br/unicesumar/projeto_1/bst/Main.java
@@ -6,16 +6,11 @@ public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         BinarySearchTree bst = new BinarySearchTree();
+
         int choice;
         int value;
-        System.out.print("qual o valor da raiz? ");
-        value = scanner.nextInt();
-        Node root = new Node(value);
 
-        System.out.println("Raiz: " + root.value);
         do {
-            Node current = root;
-
             System.out.println("\n1- Adicionar nó // 2- Procurar valor de nó // 3- Calcular altura // 4- Remover nó // 5- Mostrar arvore em ordem crescente // 0- Sair");
             System.out.print("Escreva sua ação: ");
             choice = scanner.nextInt();
@@ -23,41 +18,43 @@ public class Main {
             switch (choice) {
                 case 0:
                     break;
+
                 case 1:
                     System.out.print("Insira o valor do nó: ");
                     value = scanner.nextInt();
-                    bst.insert(value, current);
+                    bst.insert(value);
                     break;
+
                 case 2:
                     System.out.print("Insira o valor que você quer procurar: ");
                     value = scanner.nextInt();
-                    Node foundNode = bst.search(value, current);
+                    Node foundNode = bst.search(value);
                     if (foundNode != null) {
-                        System.out.println("Valor " + bst.search(value, current).value + " encontrado.");
+                        System.out.println("Valor " + foundNode.value + " encontrado.");
                     } else {
                         System.out.println("Valor " + value + " não encontrado.");
                     }
                     break;
+
                 case 3:
-                    System.out.println(bst.calculateHeight(current));
+                    System.out.println("Altura da árvore: " + bst.calculateHeight());
                     break;
+
                 case 4:
                     System.out.print("Insira o valor do nó que você quer remover: ");
                     value = scanner.nextInt();
-                    root = bst.remove(root, value);
-                    if (root == null){
-                        System.out.print("qual o valor da nova raiz? ");
-                        value = scanner.nextInt();
-                        root = new Node(value);
-                        System.out.println("Raiz: " + root.value);
-                    }
+                    bst.remove(value);
                     break;
+
                 case 5:
-                    bst.printTree(current);
+                    bst.printTree();
+                    System.out.println();
                     break;
+
                 default:
                     System.out.println("Insira um valor válido.");
             }
+
         } while (choice != 0);
     }
 }
