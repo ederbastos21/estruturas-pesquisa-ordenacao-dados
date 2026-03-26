a1478bec3bf828aaae24e6896ad404a05eaac126
eder <ederbastos.dev@gmail.com>
2026-03-26 14:36:03 -0300

    adicionar metodos search, insert e calculateHeight

--- src/main/java/br/unicesumar/projeto_1/avl/AVLTree.java
+++ src/main/java/br/unicesumar/projeto_1/avl/AVLTree.java
@@ -3,11 +3,58 @@ package br.unicesumar.projeto_1.avl;
 public class AVLTree {
     Node root;
 
-    public int height(Node node){
+    public int calculateHeight(Node node){
         if (node == null){
             return 0;
-        } else {
-            return node.height;
+        }
+
+        int leftHeight = calculateHeight(node.left);
+        int rightHeight = calculateHeight(node.right);
+
+        return 1 + Math.max(leftHeight, rightHeight);
+    }
+
+    public Node search (int key){
+        Node current = root;
+
+        while (true) {
+            if (current.key == key) {
+                return current;
+            }
+
+            if (key > current.key && current.right != null) {
+                current = current.right;
+            } else if (key < current.key && current.left != null){
+                current = current.left;
+            } else {
+                return null;
+            }
+
+        }
+    }
+
+    public void insert (int key){
+        Node current = root;
+        Node addedNode;
+        while (true){
+            if (root == null){
+                root = new Node (key);
+                break;
+            } else {
+                if (key < current.key && current.left == null){
+                    addedNode = current.left = new Node(key);
+                    break;
+                } else if (key >= current.key && current.right == null){
+                    addedNode = current.right = new Node (key);
+                    break;
+                }
+
+                if (key < current.key){
+                    current = current.left;
+                } else {
+                    current = current.right;
+                }
+            }
         }
     }
 }
