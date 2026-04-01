a9c67eed93e4c6b395fe1235a4bc780a7f3a4e53
kayser <gutobayerr@gmail.com>
2026-04-01 17:43:15 -0300

    Adicionando os arquivos de Quick Sort e Merge Sort e excluíndo pastas desnecessárias

--- src/main/java/br/unicesumar/projeto_3/bubble_sort/Main.java
+++
@@ -1,7 +0,0 @@
-package br.unicesumar.projeto_3.bubble_sort;
-
-public class Main {
-    public static void main(String[] args) {
-
-    }
-}
--- src/main/java/br/unicesumar/projeto_3/quick_sort/Main.java
+++
@@ -1,7 +0,0 @@
-package br.unicesumar.projeto_3.quick_sort;
-
-public class Main {
-    public static void main(String[] args) {
-
-    }
-}
